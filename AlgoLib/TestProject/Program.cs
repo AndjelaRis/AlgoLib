55460cbeaabd9b9fd7b7717be24869b984d43aeb
AndjelaRis <49759613+AndjelaRis@users.noreply.github.com>
2020-04-13 06:42:19 +0200

    Update Program.cs

--- AlgoLib/TestProject/Program.cs
+++ AlgoLib/TestProject/Program.cs
@@ -1,5 +1,4 @@
 ﻿using AlgoLib.Sorting;
-using AlgoLib.MatchingAlgorithms;
 
 namespace TestProject
 {
@@ -10,16 +9,6 @@ namespace TestProject
         //Radix
         static void Main(string[] args)
         {
-            string text = @"E:\text10words.txt";
-            string pattern = @"E:\pattern.txt";
-            string fileOfIndexes = @"E:\fileOfIndexes.txt";
-            //int primeNumber = 73;
-            //int numOfChar = 256;
-            //SearchAlgorithams.RabinKarp_Match(text, pattern, fileOfIndexes, numOfChar, primeNumber);
-            //SearchAlgorithams.KnuthMorrisPratt_Match(text, pattern, fileOfIndexes);
-            SearchAlgorithms.SoundEx(text, pattern, fileOfIndexes);
-
-
             //TestForBasicAlgorithms();
             //SortingPerformanceCalculator.TempTestForNElements(10, SortDirection.Increasing);
             TempTestForAll();
